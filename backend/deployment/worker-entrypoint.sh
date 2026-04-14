4601d0755f2821a3ae655ee492b29e7434be3a6d
islam0122 <duishobaevislam01@gmail.com>
2026-04-14 10:40:43 +0600

    35 --- celery

--- backend/deployment/worker-entrypoint.sh
+++ backend/deployment/worker-entrypoint.sh
@@ -4,4 +4,4 @@ if ! id -u celeryuser >/dev/null 2>&1; then
     useradd -m celeryuser
 fi
 
-exec su celeryuser -c " celery -A config worker -l info --concurrency=2"
+exec su celeryuser -c "celery -A config worker -l info --concurrency=2"
