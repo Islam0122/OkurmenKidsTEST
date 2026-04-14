1aac2d59a3303f43747f6731a5c219338b4c6304
islam0122 <duishobaevislam01@gmail.com>
2026-04-14 10:48:10 +0600

    36 --- celery

--- backend/deployment/worker-entrypoint.sh
+++ backend/deployment/worker-entrypoint.sh
@@ -4,4 +4,4 @@ if ! id -u celeryuser >/dev/null 2>&1; then
     useradd -m celeryuser
 fi
 
-exec su celeryuser -c "celery -A config worker -l info --concurrency=2"
+exec su celeryuser -c "celery -A config beat --scheduler django_celery_beat.schedulers:DatabaseScheduler"
