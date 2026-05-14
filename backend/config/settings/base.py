dad24a89e530b25f739090ce7be2ce53947cc29e
islam0122 <duishobaevislam01@gmail.com>
2026-05-14 11:48:35 +0600

    69--- fix quiz

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '50000/hour',
-    '   user': '50000/hour',
+    'user': '50000/hour',
     },
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
